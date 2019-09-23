f4310d797431546505adfc392d7e0918f017ec31
srava <srava@LAPTOP-5F90I6VO>
2019-09-22 23:01:38 -0400

    Commit for Generics

--- projectTraining/src/oopConcepts/calculateSalary.java
+++ projectTraining/src/oopConcepts/calculateSalary.java
@@ -23,8 +23,8 @@ public class calculateSalary {
 	public static void main(String args[]) 
 	{ 
 		calculateSalary s = new calculateSalary(); 
-		System.out.println(s.sum(10, 20)); 
-		System.out.println(s.sum(10, 20, 30)); 
-		System.out.println(s.sum(10.5, 20.5)); 
+		System.out.printf("Sum of x+y in first method sum \t" + s.sum(10, 20) + "\n\n"); 
+		System.out.printf("Sum of x+y+z in second method sum \t" + s.sum(10, 20, 30) + "\n\n"); 
+		System.out.printf("Sum of x+y double in second method sum \t"+  s.sum(10.5, 20.5) + "\n\n"); 
 	} 
 } 
